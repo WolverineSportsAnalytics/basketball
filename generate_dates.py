56345c2b8df9e9b6c202410f0ce39355b938d048
Your Name <evanciancio@evans-MacBook-Pro.local>
2017-10-11 02:07:58 -0400

    Add scrips to fill both new_dates and performance tables

---
+++ generate_dates.py
@@ -0,0 +1,38 @@
+import mysql.connector
+from datetime import timedelta, date
+import constants
+from bs4 import BeautifulSoup
+import urllib2
+import requests
+
+def generatedates(startDay, startMonth, startYear, endDay, endMonth, endYear, cursor):
+    date_statement = 'DECLARE @date date = '	
+    insert = "INSERT into new_dates(date) VALUES(%s)"
+    start_date = date(startYear, startMonth, startDay)
+    end_date = date(endYear, endMonth, endDay)
+    for single_date in daterange(start_date, end_date):
+	    dates =  str(single_date.year) + '-' + str(single_date.month) + '-' + str(single_date.day)
+	    new_date = date_statement + "\'" + dates +"\';" 
+	    date_stat = (dates,) 
+	    cursor.execute(insert, date_stat)
+	    print new_date
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
+if __name__ == "__main__":
+    cnx = mysql.connector.connect(user=constants.databaseUser,
+                                  host=constants.databaseHost,
+                                  database=constants.databaseName,
+                                  password=constants.databasePassword)
+    cursor = cnx.cursor(buffered=True)
+
+    #updateAndInsertPlayerRef(constants.startDayP, constants.startMonthP, constants.startYearP, constants.endDayP, constants.endMonthP, constants.endYearP, cursor )
+    generatedates(constants.startDayP, constants.startMonthP, constants.startYearP, constants.endDayP, constants.endMonthP, constants.endYearP,cursor)
+
+
+    cursor.close()
+    cnx.commit()
+    cnx.close()
