cb7d10099cde5108b1124aa2142adc6acce751fa
Your Name <evanciancio@evans-MacBook-Pro.local>
2017-10-12 22:29:43 -0400

    Update scraper to include advanced stats: doesn't include dates

--- perfomanceScraper.py
+++ perfomanceScraper.py
@@ -19,11 +19,11 @@ def generateURLs(startDay, startMonth, startYear, endDay, endMonth, endYear):
                     '&day=' + str(single_date.day) + '&year=' + str(single_date.year))
     return urls
 
-
 def updateAndInsertPlayerRef(startDay, startMonth, startYear, endDay, endMonth, endYear, cursor):
     
     # set range of dates	
-    urls = generateURLs(startDay, startMonth, startYear, endDay, endMonth, endYear)
+    # urls = generateURLs(startDay, startMonth, startYear, endDay, endMonth, endYear)
+    urls = ["https://www.basketball-reference.com/boxscores/201704070DAL.html"]
     start_date = date(startYear, startMonth, startDay)
     end_date = date(endYear, endMonth, endDay)
    
@@ -39,61 +39,114 @@ def updateAndInsertPlayerRef(startDay, startMonth, startYear, endDay, endMonth, 
     # loop through all url's 
     for url in urls:
 	get_date = select_date + dates[date_counter] + "\""
-        print get_date
+        #print get_date
 	page = requests.get(url)
         soup = BeautifulSoup(page.text, 'html.parser')
 
-        update_performance = "INSERT INTO performance (playerID, dateID, blocks, steals, points, assists, turnovers, 3PM, rebounds, doubleDouble, tripleDouble) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 	cursor.execute(get_date)
 	date_id = cursor.fetchall()[0][0]
-	print date_id
 
+	tables = soup.find_all("tbody")
+        
+	team_tables = [[tables[0],tables[1]],[tables[2],tables[3]]]
 
-        for tr in soup.find_all('tr')[1:]:
-            # first find, then updated
-            try:
-                tds = tr.find_all('td')
-                nickName = tds[0].a.text
-		get_id = selec_id + nickName  + "\""
-		cursor.execute(get_id)
-		player_id = cursor.fetchall()[0][0]
-	
-                blocks = tds[20].text
-		steals = tds[19].text
-		points = tds[23].text
-		assists = tds[18].text
-		to = tds[21].text
-		tpm = tds[10].text
-		rebounds = tds[17].text
-		triple_double = 0
-		double_double = 0
-
-
-		plus_tens = 0
-		if int(steals) > 10:
-			plus_tens += 1
-		if int(blocks) > 10:
-			plus_tens += 1
-		if int(points) > 10:	
-			plus_tens += 1
-		if int(assists) > 10:
-			plus_tens += 1
-		if int(rebounds) > 10:
-			plus_tens += 1
-
-		if plus_tens > 2:
-			triple_double = 1
-		elif plus_tens > 1:
-			double_double = 1
+	for team in team_tables:
 		
-		inserts = (player_id, date_id, blocks,steals, points, assists, to, tpm , rebounds, triple_double, double_double)
-		cursor.execute(update_performance, inserts)
-            except:
-		    pass
+		rows_1 = team[0].find_all('tr')
+		rows_2 = team[1].find_all('tr')
+		# scrape regular stats
+		for number in range(len(rows_1)):
+	            # first find, then updated
+	        	try:
+		                nickName = rows_1[number].find_all('th')[0].a.text
+	     	 		tds1 = rows_1[number].find_all('td')
+	                	minutes = tds1[0].text
+				get_id = selec_id + nickName  + "\""
+				cursor.execute(get_id)
+				player_id = cursor.fetchall()[0][0]
+	                	blocks = tds1[15].text
+				steals = tds1[14].text
+				points = tds1[18].text
+				assists = tds1[13].text
+				to = tds1[16].text
+				rebounds = tds1[12].text
+		 		triple_double = 0
+				double_double = 0
+				o_rebs = tds1[10].text
+				d_rebs = tds1[11].text
+				
+				fgs = tds1[1].text
+                        	fga = tds1[2].text
+				if fga == 0:
+					fgpercent = "NULL"
+				else:
+					fgpercent = tds1[3].text
 
-	date_counter+=1 
+				 	
+				tpm = tds1[4].text
+				tpa = tds1[5].text
+				if tpa ==0:
+					 tp_percent = "NULL"
+				else:
+					 tp_percent = tds1[6].text
+
+				free_throws = tds1[7].text
+				fta = tds1[8].text
+				if fta == 0:
+				 	ft_percent = "NULL"
+				else:
+					ft_percent = tds1[9].text
+
+				pf = tds1[17].text
+				plus_minus = tds1[18].text
+
+				# advanced statistics 
+				tds = rows_2[number].find_all('td')
+				TS = tds[1].text
+				eFG = tds[2].text
+				TPAR = tds[3].text
+				FTR= tds[4].text
+				ORBR= tds[5].text
+				DRBR= tds[6].text
+				TRBR= tds[7].text
+				ASTR= tds[8].text
+				STLR= tds[9].text
+				BLKR= tds[10].text
+				TOVR= tds[11].text
+				USGR= tds[12].text
+				ORtg= tds[13].text
+				DRtg = tds[14].text	
+
+				plus_tens = 0
+				if int(steals) > 10:
+					plus_tens += 1
+				if int(blocks) > 10:
+					plus_tens += 1
+				if int(points) > 10:	
+					plus_tens += 1
+				if int(assists) > 10:
+					plus_tens += 1
+				if int(rebounds) > 10:
+					plus_tens += 1
+	
+				if plus_tens > 2:
+					triple_double = 1
+				elif plus_tens > 1:
+					double_double = 1
 
 
+				inserts = (player_id, date_id, points, minutes, fgs, fga, fgpercent, tpm, tpa, tp_percent, free_throws, fta, ft_percent, o_rebs, d_rebs, rebounds, assists, steals, blocks, to, pf, plus_minus, TS, eFG, TPAR, FTR, ORBR, DRBR, TRBR, ASTR, STLR, BLKR, TOVR, USGR, ORtg, DRtg, triple_double, double_double)
+	
+		        	update_performance = "INSERT INTO performance (playerID, dateID, points, minutesPlayed, fieldGoals, fieldGoalsAttempted, fieldGoalPercent, 3PM, 3PA, 3PPercent, FT, FTA, FTPercent, offensiveRebounds, defensiveRebounds, totalRebounds, assists,  steals, blocks, turnovers, personalFouls, plusMinus, trueShootingPercent, effectiveFieldGoalPercent, 3pointAttemptRate, freeThrowAttemptRate, offensiveReboundPercent, defensiveReboundPercent, totalReboundPercent, assistPercent, stealPercent, blockPercent, turnoverPercent, usagePercent, offensiveRating, defensiveRating,  tripleDouble, doubleDouble) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+		
+				cursor.execute(update_performance, inserts)	
+			
+					
+	
+		#date_counter+=1 
+	
+			except:
+				pass
 if __name__ == "__main__":
     cnx = mysql.connector.connect(user=constants.databaseUser,
                                   host=constants.databaseHost,
