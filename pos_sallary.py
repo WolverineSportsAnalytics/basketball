fb25dc27e213f60cbaf23665a7274bb0bdfa8306
hbrendan <hbrendan@umich.edu>
2017-10-24 00:28:38 -0400

    matching IDs for rotoguru function

--- pos_sallary.py
+++ pos_sallary.py
@@ -7,79 +7,114 @@ import requests
 import csv
 import traceback
 def fd_pos_convert(pos):
-	if pos == '1':
-		return 'PG'
-	if pos == '2':
-		return 'SG'
-	if pos == '3':
-		return 'SF'
-	if pos == '4':
-		return 'PF'
-	if pos == '5':
-		return 'C'
+    if pos == '1':
+        return 'PG'
+    if pos == '2':
+        return 'SG'
+    if pos == '3':
+        return 'SF'
+    if pos == '4':
+        return 'PF'
+    if pos == '5':
+        return 'C'
 
 def dk_pos_convert(pos):
-	if len(pos) == 1:
-		return fd_pos_convert(pos)
-	elif len(pos) == 2:
-		return fd_pos_convert(pos[0]) + '/' + fd_pos_convert(pos[1])
+    if len(pos) == 1:
+        return fd_pos_convert(pos)
+    elif len(pos) == 2:
+        return fd_pos_convert(pos[0]) + '/' + fd_pos_convert(pos[1])
 
 def date_convert(date):
-	return date[:4] + "-" + date[4:6] + "-" + date[6:8]
+    return date[:4] + "-" + date[4:6] + "-" + date[6:8]
 
+def alignPlayerIDs(cursor):
+    selec_id = "select playerID from player_reference where firstName= %s and lastName = %s"
+    select_idTwo = "SELECT playerID from player_reference where nickName = %s"
+
+    insertRotoguruID = "UPDATE player_reference SET rotoguruID = %s WHERE playerID = %s"
+
+    with open('rotoguru20162017data.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        i = 0
+        for row in reader:
+            firstName = row[2]
+            firstName = firstName.strip()
+            lastName = row[1]
+            lastName = lastName.strip()
+            selectData = (firstName, lastName)
+            cursor.execute(selec_id, selectData)
+            if not cursor.rowcount:
+                name =""
+                name = firstName + " " + lastName
+                nameData = (name,)
+                cursor.execute(select_idTwo, nameData)
+                if cursor.rowcount:
+                    playerID = 0
+                    for id in cursor:
+                        playerID = id[0]
+                    insertData = (row[0].strip(), playerID)
+                    cursor.execute(insertRotoguruID, insertData)
+                    cnx.commit()
+            else:
+                playerID = 0
+                for id in cursor:
+                    playerID = id[0]
+                insertData = (row[0].strip(), playerID)
+                cursor.execute(insertRotoguruID, insertData)
+                cnx.commit()
 
 def scrape_rotoguru(cursor, cnx):
-	#empty will be used to scrape from rotoguru csv
-    	selec_id = "select playerID from player_reference where nickName=\""
-    	selec_date_id = "select iddates from new_dates where date=\""
- 
- 	update_performance = "Update performance set fanduel=%s, draftkings=%s, fanduelPosition=%s, draftkingsPosition=%s where playerID=%s and dateID=%s"  
-	false = []
-	with open('rotoguru20162017data.csv', 'rb') as csvfile:
-		reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-		i = 0
-		for row in reader:
-			try:
-				name = row[3]
-                	    	get_id = selec_id + name + "\""
-                	    	cursor.execute(get_id)
-                	    	player_id = cursor.fetchall()[0][0]
+    #empty will be used to scrape from rotoguru csv
+    selec_id = "select playerID from player_reference where nickName=\""
+    selec_date_id = "select iddates from new_dates where date=\""
+
+    update_performance = "Update performance set fanduel=%s, draftkings=%s, fanduelPosition=%s, draftkingsPosition=%s where playerID=%s and dateID=%s"
+    false = []
+    with open('rotoguru20162017data.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        i = 0
+        for row in reader:
+            try:
+                name = row[3]
+                get_id = selec_id + name + "\""
+                cursor.execute(get_id)
+                player_id = cursor.fetchall()[0][0]
 
 
-				date = row[4]
-				get_date = selec_date_id + date + "\""
-                	    	cursor.execute(get_date)
-                	    	date_id = cursor.fetchall()[0][0]
+                date = row[4]
+                get_date = selec_date_id + date + "\""
+                cursor.execute(get_date)
+                date_id = cursor.fetchall()[0][0]
 
 
-				dk_salary = row[25] 
-				fd_salary = row[23]
-				dk_pos = row[32]
-				dk_pos = dk_pos_convert(dk_pos)
-				fd_pos = row[31]
-				fd_pos = fd_pos_convert(fd_pos)
-				
-				inserts = (
-						fd_salary,
-						dk_salary,
-						fd_pos,
-						dk_pos,
-						player_id,
-						date_id)
-			
-				
-				cursor.execute(update_performance,inserts)	
+                dk_salary = row[25]
+                fd_salary = row[23]
+                dk_pos = row[32]
+                dk_pos = dk_pos_convert(dk_pos)
+                fd_pos = row[31]
+                fd_pos = fd_pos_convert(fd_pos)
 
-			except: 
-				traceback.print_exc()
-				false.append(name)
-
-			cnx.commit()
+                inserts = (
+                    fd_salary,
+                    dk_salary,
+                    fd_pos,
+                    dk_pos,
+                    player_id,
+                    date_id)
 
 
-	cursor.close()
-	cnx.commit()
-    	cnx.close()
+                cursor.execute(update_performance,inserts)
+
+            except:
+                traceback.print_exc()
+                false.append(name)
+
+            cnx.commit()
+
+
+    cursor.close()
+    cnx.commit()
+    cnx.close()
 '''
         # get what names don't match  
 	wrong_names= []
@@ -104,7 +139,8 @@ if __name__ == "__main__":
                                   database=constants.databaseName,
                                   password=constants.databasePassword)
     cursor = cnx.cursor(buffered=True)
+    scrape_rotoguru(cursor)
 
-    scrape_rotoguru(cursor, cnx)
-
-    
+    cursor.close()
+    cnx.commit()
+    cnx.close()
