ccde032e2d6c5e346c692f47c05945c9c1461b2e
Your Name <evanciancio@evans-MacBook-Pro.local>
2017-10-27 12:09:14 -0400

    start to scrape for all team data

--- new_performance.py
+++ new_performance.py
@@ -3,6 +3,11 @@ import mysql.connector
 from datetime import timedelta, date
 import constants
 from bs4 import BeautifulSoup, Comment
+  
+
+6@q
+ 
+
 import urllib2
 import requests
 
@@ -14,7 +19,12 @@ def updateAndInsertPlayerRef(startDay, startMonth, startYear, endDay, endMonth, 
     urls = ["https://www.basketball-reference.com/boxscores/201704070DAL.html"]
     start_date = date(startYear, startMonth, startDay)
     end_date = date(endYear, endMonth, endDay)
-   
+
+    insert_team_data = "INSERT INTO basketball.team_performance (dailyTeamID, dailyTeamOpponentID, dateID, pointsAllowed, pointsScored, pace, effectiveFieldGoalPercent, turnoverPercent, FT/FGA, offensiveRating, defensiveRating, FG, FGA, FGP, 3P, 3PA, 3PP, FT, FTA, FTP, offensiveRebounds, defensiveRebounds, totalebounds, assists, steal, blocks, turnovers, personalFouls, trueShootingPercent, 3pointAttemptRate, freeThrowAttemptRate, defensiveReboundPercent, offensiveReboundPercent, totalReboundPercent, assistPercent, stealPercent, blockPercent) VALUES( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+
+
+
 	
     # loop through all url's 
     for url in urls:
@@ -25,13 +35,17 @@ def updateAndInsertPlayerRef(startDay, startMonth, startYear, endDay, endMonth, 
 	comments = soup.findAll(text=lambda text:isinstance(text, Comment))
 	comment = comments[len(comments) - 23]
 
+        teams_data = [[],[]]
 	soup1 = BeautifulSoup(comment, "html.parser")
+        team_num = 0
+
 	for tr in soup1.find_all('tr')[2:]:
 		
 		#team name
 		team = tr.find_all('th')[0].text
 	
 		#table columns
+
 		tds = tr.find_all('td')
 		pace = tds[0].text
 		eShooting = tds[1].text
@@ -39,18 +53,30 @@ def updateAndInsertPlayerRef(startDay, startMonth, startYear, endDay, endMonth, 
 		oReboundPercent = tds[3].text
 		FTOverFQAttempts = tds[4].text
 		ORTG = tds[5].text
+                
+                teams_data[team_num].append(team)
+                teams_data[team_num].append(pace)
+                teams_data[team_num].append(eShooting)
+                teams_data[team_num].append(turnoverPercent)
+                teams_data[team_num].append(oReboundPercent)
+                teams_data[team_num].append(FTOverFQAttempts)
+                teams_data[team_num].append(ORTG)
+                team_num=+1
+                
 
-		print pace, eShooting, turnoverPercent, oReboundPercent, FTOverFQAttempts, ORTG
 	
+	team_num = 0
+        for tfoot in soup.find_all('tfoot'):
+            td = tfoot.find_all('td')
+            for i in td:
+                teams_data[team_num/2].append(i.text)
+            team_num+=1
 
-	for tr in soup.find_all('tr')[2:]:
-	#	print tr
-            # first find, then updated
-                tds = tr.find_all('td')
-         #       print tds[1].a.text
-	    
-		    
+        teams_data[0].append(teams_data[1][0])
+        teams_data[1].append(teams_data[0][0])
 
+        for team in teams_data:
+            
 
 if __name__ == "__main__":
     cnx = mysql.connector.connect(user=constants.databaseUser,
