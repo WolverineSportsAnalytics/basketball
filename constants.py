82eb3bd10b50f68d85afd4c926bf399b865fcb76
hbrendan <hbrendan@umich.edu>
2017-11-27 21:08:45 -0500

    lets Ride

--- constants.py
+++ constants.py
@@ -3,24 +3,24 @@ databaseUser='wsa@wsabasketball'
 databaseName='basketball'
 databasePassword='LeBron>MJ!'
 
-yearP = 2016
-monthP = 12
-dayP = 21
+yearP = 2017
+monthP = 11
+dayP = 22
 
 BasketballRefPerGameData='https://www.basketball-reference.com/friv/dailyleaders.fcgi?month=04&day=13&year=2017'
 BasketballRefBoxScoreBase = 'https://www.basketball-reference.com/boxscores/'
 
 startYearP = 2017
-startMonthP = 10
-startDayP = 17
+startMonthP = 11
+startDayP = 22
 
 endYearP = 2017
 endMonthP = 11
 endDayP = 20
 
-gdStartYear = 2016
-gdStartMonth = 12
-gdStartDay = 17
+gdStartYear = 2017
+gdStartMonth = 2
+gdStartDay = 10
 
 dailyPerformanceExtrapolationDateCutOff = 883
 dailyPerformanceLastTableID = 1
@@ -29,4 +29,8 @@ teamPerformanceExtrapolationDateCutOff = 883
 
 teamVsDefenseExtrapolationDateCutOff = 883
 
-numdaysGradientDescent = 20
+numdaysGradientDescent = 1
+
+rotoguruFileLocation = '/Users/Brendan/Google Drive/Wolverine Sports Analytics/Algorithms/Data/rotoguru20172018data.csv'
+
+fanduelFileLocation = '/Users/Brendan/Downloads/FanDuel-NBA-2017-11-22-22082-players-list (2).csv'
