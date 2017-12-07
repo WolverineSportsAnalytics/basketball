4ab0e028645958c706490f3f91b96b63f29305df
hbrendan <hbrendan@umich.edu>
2017-12-07 03:25:54 -0500

    constants

---
+++ constants.py
@@ -0,0 +1,42 @@
+# database data
+
+databaseHost='wsabasketball.mysql.database.azure.com'
+databaseUser='wsa@wsabasketball'
+databaseName='basketball'
+databasePassword='LeBron>MJ!'
+
+# scraping data
+BasketballRefPerGameData='https://www.basketball-reference.com/friv/dailyleaders.fcgi?month=04&day=13&year=2017'
+BasketballRefBoxScoreBase = 'https://www.basketball-reference.com/boxscores/'
+
+rotoguruFileLocation = '/Users/Brendan/Google Drive/Wolverine Sports Analytics/Algorithms/Data/rotoguru20172018data.csv'
+
+fanduelFileLocation = '/Users/Brendan/Downloads/FanDuel-NBA-2017-12-05-22284-players-list.csv'
+
+# predicting data
+yearP = 2017
+monthP = 12
+dayP = 6
+
+startYearP = 2017
+startMonthP = 12
+startDayP = 5
+
+endYearP = 2017
+endMonthP = 12
+endDayP = 6
+
+gdStartYear = 2017
+gdStartMonth = 11
+gdStartDay = 26
+
+numdaysGradientDescent = 700
+
+# extrapolation constraints
+dailyPerformanceExtrapolationDateCutOff = 933
+
+teamPerformanceExtrapolationDateCutOff = 933
+
+teamVsDefenseExtrapolationDateCutOff = 933
+
+extapolatorUpperBound = 933
