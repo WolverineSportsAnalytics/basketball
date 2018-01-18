6401def8a163c2d888374703882b6136b7c0d585
phillipmathew314 <phillipmathew314@gmail.com>
2018-01-18 16:40:42 -0500

    moneyLineScraper prints out the team and the odds

--- moneyLineScraper.py
+++ moneyLineScraper.py
@@ -2,7 +2,7 @@ import mysql.connector
 from datetime import timedelta, date
 import constants
 from bs4 import BeautifulSoup
-import urllib2
+import urllib
 import requests
 
 def daterange(start_date, end_date):
@@ -14,10 +14,15 @@ def generateURLs(startDay, startMonth, startYear, endDay, endMonth, endYear):
     end_date = date(endYear, endMonth, endDay)
     urls = []
     for single_date in daterange(start_date, end_date):
-        urls.append('https://www.sportsbookreview.com/betting-odds/nba-basketball/?date=' + str(single_date.year)) + str(single_date.month) + str(single_date.day))
+        if single_date.day > 9:
+            urls.append(
+                'https://www.sportsbookreview.com/betting-odds/nba-basketball/?date=' + str(single_date.year) + str(
+                    single_date.month) + str(single_date.day))
+        else:
+            urls.append(
+                'https://www.sportsbookreview.com/betting-odds/nba-basketball/?date=' + str(single_date.year) + str(
+                    single_date.month) + '0' + str(single_date.day))
     return urls
-
-#https://www.sportsbookreview.com/betting-odds/nba-basketball/?date=20171011
 
 def InsertGameOdds(startDay, startMonth, startYear, endDay, endMonth, endYear):
     urls = generateURLs(startDay, startMonth, startYear, endDay, endMonth, endYear)
@@ -30,41 +35,39 @@ def InsertGameOdds(startDay, startMonth, startYear, endDay, endMonth, endYear):
 
     for url in urls:
 
+        print(url)
         page = requests.get(url)
         soup = BeautifulSoup(page.text, 'html.parser')
 
-        addGame = "INSERT INTO game_odds (homeID, awayID, homeSpread, awaySpread, homeMoneyLine, awayMoneyLine) VALUES(%s, %s, %s, %s, %s, %s)"
+        #addGame = "INSERT INTO game_odds (homeID, awayID, homeSpread, awaySpread, homeMoneyLine, awayMoneyLine) VALUES(%s, %s, %s, %s, %s, %s)"
+        addTeamID = "INSERT INTO game_odds (homeID, awayID) VALUES(%s, %s)"
 
-        for tr in soup.find_all("div", { "class" : "status" })[1:]:
+        for team in soup.find_all('span', attrs={'class': 'team-name'})[:]:
             # first find, then update
+            print(team.text)l
 
+
+        for odds in soup.find_all('div', attrs={'class': 'eventLine-book-value'})[:]:
+            print(odds.text)
+
+            """
             try:
-                tds = tr.find_all('')       #FIX THIS STUFF
-                nickName = tds[0].a.text
-                lastName = tds[0]['csk'].split(",")[0]
-                firstName = tds[0]['csk'].split(",")[1]
-                bbref = tds[0]['data-append-csv']
-                team = tds[1].a.text
+                allDivs = game.find_all("div")[1:]       #FIX THIS STUFF
+                print(allDivs[0].text)
+                #names = (homeID, awayID, homeSpread, awaySpread, homeMoneyLine, awayMoneyLine)
+                #cursor.execute(addGame, names)
 
-                if cursor.rowcount == 0:
-                    # add because did not find anything
-                    names = (nickName, bbref, firstName, lastName, team)
-                    cursor.execute(addPlayer, names)
-                else:
-                    # update
-                    updatePQ = (team, bbref)
-                    cursor.execute(updatePlayer, updatePQ)
             except:
-                pass
+                pass"""
 
         cnx.commit()
 
-        print "Updated Historical Odds in game_odds for URL: " + str(url)
+        print("Updated Historical Odds in game_odds for URL: " + str(url))
 
     cursor.close()
     cnx.commit()
     cnx.close()
 
 if __name__ == "__main__":
-    updateAndInsertPlayerRef(constants.startDayP, constants.startMonthP, constants.startYearP,
+    InsertGameOdds(constants.startDayP, constants.startMonthP, constants.startYearP,
                              constants.endDayP, constants.endMonthP, constants.endYearP)
