9b038032b9be08c73329f3c27cb3df233ac9281a
hbrendan <hbrendan@umich.edu>
2018-03-09 02:46:02 -0500

    filled in the gaps to readme, fixed sumpoints + projMinutes so no errors in futures filling

--- sqlScripts/sumPoints.sql
+++ sqlScripts/sumPoints.sql
@@ -1,2 +1,4 @@
 update basketball.performance set draftkingsPts = (points + 3PM*.5 + totalRebounds*1.25 + steals*2 + blocks*2 -turnovers*.5 + doubleDouble + tripleDouble) where dateID>850;
 update basketball.performance set fanduelPts = (FT + totalRebounds*1.2 + assists*1.5 + blocks*3 + steals*3 - turnovers + (3PM)*3 + (fieldGoals-3PM)*2) where dateID>850;
+
+update basketball.perfomrance set minutesPlayed = projMinutes where projMinutes IS NULL
