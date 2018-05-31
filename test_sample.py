d2c70706c0b86e1ade852ee92ad4e4206eee14ef
Evan Ciancio <eciancio@llnw.com>
2018-05-31 14:31:40 -0400

    Please work

--- test_sample.py
+++ test_sample.py
@@ -1,8 +1,10 @@
 import unittest
+import models
 
 class MyTest(unittest.TestCase):
     def test(self):
-        self.assertEqual(1,1,)
+        dog = models.benSimmonsModel
+        self.assertEqual(1,1)
 
 if __name__ == '__main__':
         unittest.main()
