80ade0a54dfb926a8d8295d9868d438682cc5aad
Evan Ciancio <ciancioe@gmail.com>
2018-10-09 20:53:47 -0400

    Clear

--- sqlScripts/seeCurrentPlayersPlayingOnDate.sql
+++
@@ -1,5 +0,0 @@
-SELECT b.nickName, p.playerID, p.dateID, p.fanduel, p.minutesPlayed,
-p.team, p.opponent, p.fanduelPosition, p.fanduelPts, p.fdPointsSKLinPredRidge, p.projMinutes
-FROM basketball.performance as p
-INNER JOIN basketball.player_reference as b ON b.playerID = p.playerID
-WHERE p.dateID = 929;
--- sqlScripts/updateFanDuelPositions.sql
+++
@@ -1,9 +0,0 @@
-UPDATE performance
-SET fanduelPosition = 'SG'
-WHERE playerID = 1772;
-
-SELECT DISTINCT p.playerID, r.nickName, p.fanduelPosition 
-FROM basketball.performance p
-INNER JOIN player_reference r 
-ON r.playerID = p.playerID
-WHERE p.fanduelPosition IS NULL;
