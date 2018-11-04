b753920abb9659bdb6b17b9dd5ba169d59c74c40
Evan Ciancio <ciancioe@gmail.com>
2018-11-04 11:51:14 -0500

    Added cross val stub

---
+++ MachineLearning/crossValidation.py
@@ -0,0 +1,20 @@
+def get_features_matrix(cnx, cursor, start_date, end_date):
+    '''
+    Will connect to database and get all the features for all players in between 
+    start_date and end_date, which are date id's
+    Returns matrix of all players there features and final performance
+    '''
+    pass
+
+def cross_validation(model, train_x, train_y, k):
+    '''
+    Takes in a model with set hyperperameters and train_x and train_y
+    Will run k fold cross validation and return perfomance metrics (r2)
+    can take in any model
+    '''
+    pass
+
+
+def main():
+    # call them in here
+    pass
