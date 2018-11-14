f80cab724c601ccd982bccf77140b80b01fe641d
Evan Ciancio <ciancioe@gmail.com>
2018-11-13 20:24:08 -0500

    Add automation.py comments

---
+++ automation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' Automate of running WSA Engine '''
+
+def main():
+    ''' Script to automate running of all basketball engine daily '''
+    run_scrapers()
+
+def run_scrapers():
+    ''' Run the scrapers '''
+    cursor = _
+    cnx = _
+    dateID = _
+    today = _
+
+    # run player reference scaper
+
+    # run generate box score urls
+
+    # run performance 
+
+    # run team performance
+
+    # 3 Extrapilators
+
+    # fandual scraper 
+
+    # daily minutes
+
+    # machine learning stuff
+
+    pass
