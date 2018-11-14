8ef9f01b96028e850d2e869a1603ae62a2431043
Evan Ciancio <ciancioe@gmail.com>
2018-11-13 20:40:10 -0500

    added more code

--- automation.py
+++ automation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 ''' Automate of running WSA Engine '''
+import mysql.connector
+import datetime
 
 def main():
     ''' Script to automate running of all basketball engine daily '''
@@ -7,10 +9,27 @@ def main():
 
 def run_scrapers():
     ''' Run the scrapers '''
-    cursor = _
-    cnx = _
-    dateID = _
-    today = _
+    cnx = mysql.connector.connect(user="wsa@wsabasketball",
+                                  host='wsabasketball.mysql.database.azure.com',
+                                  database="basketball",
+                                  password="")
+    cursor = cnx.cursor(buffered=True)
+
+    cursor = cnx.cursor()    
+    now = datetime.datetime.now()
+
+    print str(now.year)+ "-" + str(now.month) + "-" + str(now.day)
+    date =  str(now.year)+ "-" + str(now.month) + "-" + str(now.day)
+    
+
+    # first figure out what day it is through some way
+    # get current date 
+    findGame = "SELECT iddates FROM new_dates WHERE date = %s"
+    findGameData = (date,)
+    # then query database to get that dateI
+    cursor.execute(findGame, findGameData) 
+    dateID = cursor.fetchall()[0][0]
+    
 
     # run player reference scaper
 
