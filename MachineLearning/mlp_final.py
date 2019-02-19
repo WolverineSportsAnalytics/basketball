3e159edc8cb70b278a2b67a4897489f7dcb500a8
Mona Lisa <coopjame@umich.edu>
2019-02-18 19:56:50 -0500

    Fix mlp regressor

--- MachineLearning/mlp_final.py
+++ MachineLearning/mlp_final.py
@@ -4,47 +4,52 @@ from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.preprocessing import scale
 import numpy as np
 from sklearn.metrics import mean_squared_error, make_scorer, explained_variance_score, mean_absolute_error
+import mysql.connector
 import pandas as pd
 import models
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import normalize
+import pickle
 
 class MLPRegressor(Algorithm):
-    def split_features(self, chosenFeatures):
-    #first, remove the draft_kings points (not needed)
-    draft_kings = [features.pop(-1) for features in self.features]
+    def split_data(self):
+        return
+    def split_features(self):
+        self.features = self.data
+        #first, remove the draft_kings points (not needed)
+        draft_kings = [features.pop(-1) for features in self.features]
 
-    #extract and remove the fanduel points (response variable)
-    fanduel = [features.pop(-1) for features in self.features]
-    
-    for i in range(5):
-        temp = [features.pop(0) for features in self.features]
+        #extract and remove the fanduel points (response variable)
+        self.target= [features.pop(-1) for features in self.features]
         
-    self.features = scale(self.features)
+        for i in range(5):
+            temp = [features.pop(0) for features in self.features]
+            
+        self.features = scale(self.features)
 
-    #return only the chosen features from features table, and the fanduel points
-    return self.features[:, chosenFeatures], fanduel
-
-    def run_pca(X_train, X_test):
-    '''applies Principal Component Analysis on data with 201 components, returns dimension-reduced training set'''
+    def run_pca(self):
+        '''applies Principal Component Analysis on data with 201 components, returns dimension-reduced training set'''
    
-        
-        pca = PCA(n_components=201)
-        pca.fit(X_train)
-        X_train_pca = pca.transform(X_train)
-        X_test_pca = pca.transform(X_test)
-
-        return X_train_pca, X_test_pca  
-
+        filename = 'PCA_model.sav'
+        pca = pickle.load(open(filename, 'rb'))
+        pca.fit(self.features)
+        self.features = pca.transform(self.features)
+        print(len(self.features[0]))
 
     def predict(self):
-        self.model = Ridge(100) # set model
-        self.model.fit(self.features, self.target) # fit model
+        self.split_features()
+        #self.run_pca()
+        filename = 'MLP_model.sav'
+        self.model = pickle.load(open(filename, 'rb'))
         self.predictions = self.model.predict(self.features) # predict for features
         return self.predictions
 
     def mse(self):
         return mean_squared_error(self.target, self.predictions)
+
+    def compare(self):
+        for i in range(len(self.predictions)):
+            print self.predictions[i], self.target[i]
 
 
 def test():
@@ -55,16 +60,19 @@ def test():
                                   password="")
     cursor = cnx.cursor(buffered=True)
 
-    getAllData = "select * from futures where fanduelPts is not null"
+    getAllData = "select * from futures where fanduelPts is not null and dateID = 899;"
     cursor.execute(getAllData)
 
     features = [list(feature) for feature in cursor.fetchall()]
+    print len(features)
+    print(len(features[0]))
 
     # How you would import and us ridge regression 
-    ridge = ridgeRegression(features)
-    predictions = ridge.predict()
+    mlp = MLPRegressor(features)
+    predictions = mlp.predict()
+    mlp.compare()
     print predictions
-    print ridge.mse()
+    print mlp.mse()
 
 if __name__=="__main__":
     test()
--- MachineLearning/mlp_regressor_tuning.py
+++ MachineLearning/mlp_regressor_tuning.py
@@ -2,42 +2,30 @@ import mysql.connector
 import pca_step1_modelworkflow
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import GridSearchCV
-
 import sklearn
-
 from sklearn.preprocessing import scale
-
 import numpy as np
-
 from sklearn.metrics import mean_squared_error, make_scorer, explained_variance_score, mean_absolute_error
-
 import pandas as pd
-
 import models
+import pickle
 from sklearn.decomposition import PCA
-
 from sklearn.preprocessing import normalize
 
 
 def train_model():
-
     '''trains an MLP Regressor model on PCA-reduced data and tunes the hyperparameters. Returns the model'''
     cnx = mysql.connector.connect(user="root",
-
                                   host="127.0.0.1",
-
                                   database="basketball",
-
-                                  password="Federer123!")
+                                  password="")
     cursor = cnx.cursor(buffered=True)
-
 
     #extract features from database
     features = pca_step1_modelworkflow.get_features_matrix(cnx, cursor, 0, 900)
 
-
     #get features and response variable
-    features, response = pca_step1_modelworkflow.split_features(features, np.linspace(0, 450, dtype=int, num=450)); #456 relevant features after removal of fanduel and draftkings
+    features, response = pca_step1_modelworkflow.split_features(features, np.linspace(0, 450, dtype=int, num=451)); #456 relevant features after removal of fanduel and draftkings
     
   
     #run pca to get sparse representation of data
@@ -46,6 +34,7 @@ def train_model():
     features_test= features[14000:]
     response_test= response[14000:]
     features_train_pca, features_test_pca = pca_step1_modelworkflow.run_pca(features_train, features_test);
+
     
 
     #Run Ridge without Feature Selection to get Variable Importance
@@ -64,6 +53,10 @@ def train_model():
    
     model = MLPRegressor(hidden_layer_sizes = (50, ), alpha = 100.0, activation = 'relu', learning_rate = 'adaptive', solver = 'sgd', max_iter = 200);
     model.fit(features_train_pca, response_train)
+
+    filename = 'MLP_model.sav'
+    pickle.dump(model, open(filename, 'wb'))
+    
     y_pred1 = model.predict(features_test_pca) #test prediction
     mses.append(mean_squared_error(y_pred1, response_test))
 
@@ -84,17 +77,9 @@ def cross_validation(clf, train_x, train_y, k):
     '''
 
     k_fold = KFold(5, shuffle = True)
-
     scores = cross_val_score(clf, train_x, train_y, cv=k_fold, n_jobs=1, scoring='neg_mean_squared_error')
 
-    return scores
-
-
-
-
-
 def main():
-
    train_model();
     
 
