73355338c4eefba5fe1d643817040735b574b1e6
Mona Lisa <lissjust@umich.edu>
2019-02-18 20:27:01 -0500

    Its even less, but the wrok is more honest

--- MachineLearning/crossValidation.py
+++ MachineLearning/crossValidation.py
@@ -4,7 +4,6 @@ def get_features_matrix(cnx, cursor, start_date, end_date):
     start_date and end_date, which are date id's
     Returns matrix of all players there features and final performance
     '''
-    pass
 
 
 def cross_validation(model, train_x, train_y, k):
@@ -13,7 +12,6 @@ def cross_validation(model, train_x, train_y, k):
     Will run k fold cross validation and return perfomance metrics (r2)
     can take in any model
     '''
-    pass
 
 
 def split_features(featuresMatrix, chosenFeatures):
--- MachineLearning/trainmodel_step2_modelworkflow.py
+++ MachineLearning/trainmodel_step2_modelworkflow.py
@@ -1,22 +1,14 @@
 import mysql.connector
 import pca_step1_modelworkflow
 
-import sklearn
 
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
 
-from sklearn.preprocessing import scale
 
 import numpy as np
 
-from sklearn.metrics import mean_squared_error, make_scorer, explained_variance_score, mean_absolute_error
 
-import pandas as pd
 
-import models
-from sklearn.decomposition import PCA
 
-from sklearn.preprocessing import normalize
 
 
 def train_model():
