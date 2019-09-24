c59738342d17cb6781d3fad69e18139e81bc7f45
justinliss <justin.d.liss@gmail.com>
2019-09-23 22:25:46 -0400

    Small changes that need to be updated like the SQL connection

---
+++ MachineLearning/project.py
@@ -0,0 +1,66 @@
+from ridgeRegression import ridgeRegression
+'''from Scrapers import performance'''
+import mysql.connector
+
+
+def projectById(startdateid, enddateid, cursor):
+    getAllData = "select * from futures where fanduelPts is not null and dateID < " + \
+        enddateid + " and dateID >= " + startdateid + " ;"
+    cursor.execute(getAllData)
+
+    # matrix where each row is a feature for each player
+    features = [list(feature) for feature in cursor.fetchall()]
+
+    # How you would import and use ridge regression
+    ridge = ridgeRegression(features)
+    # returns array of fanduel pt predictions mapping one to one with features
+    predictions = ridge.predict()
+
+    # new object with list of tuples of (playerid, dateid, projection)
+    result = []
+    for i in range(len(predictions)):
+        result.append((features[i][1], features[i][2], predictions[i]))
+
+    return result
+
+
+def insertproject(predictions, nameOfCol, cursor):
+    for tuples in predictions:
+        insertData = "UPDATE performance SET " + nameOfCol + " = " + \
+            tuples[2] + " WHERE playerID = " + tuples[0] + " AND dateID = " + tuples[1] + ";"
+        cursor.execute(insertData)
+
+
+def project(
+        startDay,
+        startMonth,
+        startYear,
+        endDay,
+        endMonth,
+        endYear,
+        cursor):
+
+    '''startid = performance.getDate(startDay, startMonth, startYear, cursor)
+    endid = performance.getDate(endDay, endMonth, endYear, cursor)
+    '''
+    cnx = mysql.connector.connect(user="wsa",
+                                  host="34.68.250.121",
+                                  database="basketball",
+                                  password="test")
+    cursor = cnx.cursor(buffered=True)
+
+    startid = 693
+    endid = 694
+    prediction = projectById(startid, endid, cursor)
+
+    # prediction returns tuples of (playerid, dateid, prediction)
+
+    nameOfModel = "ridge"
+    check = "SELECT " + nameOfModel + \
+        " FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA =basketball AND TABLE_NAME =performance;"
+    '''addCol = "ALTER TABLE performance ADD " + nameOfModel + " float;"'''
+
+    cursor.execute(check)
+    cursor.execute(addCol)
+
+    insertproject(prediction, nameOfModel, cursor)
---
+++ MachineLearning/ridge_final.py
@@ -0,0 +1,82 @@
+from algorithmClass import Algorithm
+import sklearn
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.preprocessing import scale
+import numpy as np
+from sklearn.metrics import mean_squared_error, make_scorer, explained_variance_score, mean_absolute_error
+import mysql.connector
+import pandas as pd
+import models
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import normalize
+import pickle
+
+
+class RidgeRegressor(Algorithm):
+    def split_data(self):
+        return
+
+    def split_features(self):
+        self.features = self.data
+        # first, remove the draft_kings points (not needed)
+        draft_kings = [features.pop(-1) for features in self.features]
+
+        # extract and remove the fanduel points (response variable)
+        self.target = [features.pop(-1) for features in self.features]
+
+        for i in range(5):
+            temp = [features.pop(0) for features in self.features]
+
+        self.features = scale(self.features)
+
+    def run_pca(self):
+        '''applies Principal Component Analysis on data with 201 components, returns dimension-reduced training set'''
+
+        filename = 'PCA_model.sav'
+        pca = pickle.load(open(filename, 'rb'))
+        pca.fit(self.features)
+        self.features = pca.transform(self.features)
+        print(len(self.features[0]))
+
+    def predict(self):
+        self.split_features()
+        # self.run_pca()
+        filename = 'Ridge100_model.sav'
+        self.model = pickle.load(open(filename, 'rb'))
+        self.predictions = self.model.predict(
+            self.features)  # predict for features
+        return self.predictions
+
+    def mse(self):
+        return mean_squared_error(self.target, self.predictions)
+
+    def compare(self):
+        for i in range(len(self.predictions)):
+            print self.predictions[i], self.target[i]
+
+
+def test():
+    ''' This tests and shows how predictions occur using the ridge regression object '''
+    cnx = mysql.connector.connect(user="root",
+                                  host="127.0.0.1",
+                                  database="basketball",
+                                  password="Federer123!")
+    cursor = cnx.cursor(buffered=True)
+
+    getAllData = "select * from futures where fanduelPts is not null and dateID = 899;"
+    cursor.execute(getAllData)
+
+    features = [list(feature) for feature in cursor.fetchall()]
+    print len(features)
+    print(len(features[0]))
+
+    # How you would import and us ridge regression
+    ridge = RidgeRegressor(features)
+    predictions = ridge.predict()
+    ridge.compare()
+    print predictions
+    print ridge.mse()
+
+
+if __name__ == "__main__":
+    test()
